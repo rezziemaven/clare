a7324005beb4a6d63b295b5e17330fc529a6b2b8
Sherezz Grant <rezziemaven@gmail.com>
2026-06-23 15:20:28 -0400

    Rename query to CREATE_USERS_TABLE This commit creates a CREATE_USERS_TABLE query that lives outside of the user_model function so that it can be reusable between the user model and in setting up the test users table.

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,14 +1,13 @@
 import sqlite3
 
-def user_model() -> sqlite3.Cursor | None:
-    query = """CREATE TABLE IF NOT EXISTS user (
+CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY,
         name TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL,
         date_last_period DATE NOT NULL,
-        rest_offset_before INTEGER DEFAULT "5" NOT NULL,
-        rest_offset_after INTEGER DEFAULT "5" NOT NULL,
+        rest_offset_before INTEGER DEFAULT "5",
+        rest_offset_after INTEGER DEFAULT "5",
         calendar_url TEXT
         );"""
 
